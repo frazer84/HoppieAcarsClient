c6ea25e16b667e6b5ecea87de600e609ea333856
Daniel Zarins (Hemma) <daniel@frazer.se>
2020-05-16 23:52:33 +0200

    0.4.2-alpha Added the option to include a response message ID when creating a new CpdlcAcarsMessage Added STANDBY response type

--- HoppieAcarsClient/CpdlcAcarsMessage.cs
+++ HoppieAcarsClient/CpdlcAcarsMessage.cs
@@ -12,6 +12,7 @@ namespace HoppieAcarsClient
         public CPDLCUplinkMessageType UplinkMessageType { get; private set; }
         public int MessageCount { get; private set; }
         public string Message { get; private set; }
+        public int MessageResponseId { get; private set; }
 
         /// <summary>
         /// Use to create a new CPDLC Message object
@@ -33,10 +34,41 @@ namespace HoppieAcarsClient
                     from,
                     to,
                     MessageType.CPDLC,
-                    GetDataString(messageCount, responseType, messageType))
+                    GetDataString(messageCount, 0, responseType, messageType))
         {
             ResponseType = responseType;
             MessageCount = messageCount;
+            UplinkMessageType = messageType;
+            Message = getAtcMessage(messageType);
+        }
+
+        /// <summary>
+        /// Use to create a new CPDLC Message object
+        /// </summary>
+        /// <param name="recievedAt">When the message was recieved by the client (not when it was created or sent from the sender station)</param>
+        /// <param name="from">Callsign from which the message was recieved</param>
+        /// <param name="to">Callsign to which the message was addressed</param>
+        /// <param name="responseType">Expected response type</param>
+        /// <param name="messageCount">Message counter, increases for every new message sent</param>
+        /// <param name="messageResponseId">Id of the message that are being responded to</param>
+        /// <param name="message">The ATC instruction</param>
+        public CpdlcAcarsMessage(
+            string from,
+            string to,
+            ResponseAttribute responseType,
+            int messageCount,
+            int messageResponseId,
+            CPDLCUplinkMessageType messageType) :
+                base(
+                    DateTime.Now,
+                    from,
+                    to,
+                    MessageType.CPDLC,
+                    GetDataString(messageCount, messageResponseId, responseType, messageType))
+        {
+            ResponseType = responseType;
+            MessageCount = messageCount;
+            MessageResponseId = messageResponseId;
             UplinkMessageType = messageType;
             Message = getAtcMessage(messageType);
         }
@@ -62,11 +94,43 @@ namespace HoppieAcarsClient
                     from, 
                     to, 
                     MessageType.CPDLC, 
-                    GetDataString(messageCount, responseType, message))
+                    GetDataString(messageCount, 0, responseType, message))
         {
             ResponseType = responseType;
             UplinkMessageType = CPDLCUplinkMessageType.NONE;
             MessageCount = messageCount;
+            Message = message;
+        }
+
+        /// <summary>
+        /// Use to create a new CPDLC Message object
+        /// </summary>
+        /// <param name="recievedAt">When the message was recieved by the client (not when it was created or sent from the sender station)</param>
+        /// <param name="from">Callsign from which the message was recieved</param>
+        /// <param name="to">Callsign to which the message was addressed</param>
+        /// <param name="responseType">Expected response type</param>
+        /// <param name="messageCount">Message counter, increases for every new message sent</param>
+        /// <param name="messageResponseId">Id of the message that are being responded to</param>
+        /// <param name="message">The ATC instruction</param>
+        public CpdlcAcarsMessage(
+            DateTime recievedAt,
+            string from,
+            string to,
+            ResponseAttribute responseType,
+            int messageCount,
+            int messageResponseId,
+            string message) :
+                base(
+                    recievedAt,
+                    from,
+                    to,
+                    MessageType.CPDLC,
+                    GetDataString(messageCount, messageResponseId, responseType, message))
+        {
+            ResponseType = responseType;
+            UplinkMessageType = CPDLCUplinkMessageType.NONE;
+            MessageCount = messageCount;
+            MessageResponseId = messageResponseId;
             Message = message;
         }
 
@@ -99,19 +163,21 @@ namespace HoppieAcarsClient
 
         private static string GetDataString(
             int messageCount,
+            int responseMessageId,
             ResponseAttribute responseType,
             CPDLCUplinkMessageType messageType)
         {            
-            return GetDataString(messageCount, responseType, getAtcMessage(messageType));
+            return GetDataString(messageCount, responseMessageId, responseType, getAtcMessage(messageType));
         }
 
         private static string GetDataString(
             int messageCount,
+            int responseMessageId,
             ResponseAttribute responseType,
             string message)
         {
             // Example: "/data2/17//WU/CROSS @SPL@ AT @5000"
-            string dataString = String.Format("/data2/{0}//{1}/{2}", (messageCount > 0 ? messageCount.ToString() : ""), responseAttributeStrings[responseType], message);
+            string dataString = String.Format("/data2/{0}/{1}/{2}/{3}", (messageCount > 0 ? messageCount.ToString() : ""), (responseMessageId > 0 ? responseMessageId.ToString() : ""), responseAttributeStrings[responseType], message);
 
             return dataString;
         }
@@ -139,7 +205,20 @@ namespace HoppieAcarsClient
 
         private static string getAtcMessage(CPDLCUplinkMessageType messageType)
         {
-            return messageType.ToString().Replace('_', ' '); //TODO: Add support for more types
+            // / data2 / 33 / 1 / NE / UNABLE
+            // / data2 / 22 / 4 / N / WILCO
+            switch (messageType)
+            {
+                case CPDLCUplinkMessageType.RESPONSE_AFFIRM:
+                case CPDLCUplinkMessageType.RESPONSE_NEGATIVE:
+                case CPDLCUplinkMessageType.RESPONSE_ROGER:
+                case CPDLCUplinkMessageType.RESPONSE_STANDBY:
+                case CPDLCUplinkMessageType.RESPONSE_UNABLE:
+                case CPDLCUplinkMessageType.RESPONSE_WILCO:
+                    return messageType.ToString().Replace("RESPONSE_", "");
+                default:
+                    return messageType.ToString().Replace('_', ' ');
+            }
         }
 
         private static readonly Dictionary<ResponseAttribute, string> responseAttributeStrings = new Dictionary<ResponseAttribute, string>
@@ -164,7 +243,7 @@ namespace HoppieAcarsClient
         {
             get
             {
-                return GetDataString(MessageCount, ResponseType, UplinkMessageType);
+                return GetDataString(MessageCount, MessageResponseId, ResponseType, UplinkMessageType);
             }
         }
 
@@ -173,12 +252,12 @@ namespace HoppieAcarsClient
             NONE = 0,
             REQUEST_LOGON = 1,
             LOGOFF = 2,
-
             RESPONSE_WILCO = 100,
             RESPONSE_UNABLE = 101,
-            RESPONSE_AFFIRM = 102,
-            RESPONSE_NEGATIVE = 103,
-            RESPONSE_ROGER = 104
+            RESPONSE_STANDBY = 102,
+            RESPONSE_ROGER = 103,
+            RESPONSE_AFFIRM = 104,
+            RESPONSE_NEGATIVE = 105
         }
 
         public enum ResponseAttribute
